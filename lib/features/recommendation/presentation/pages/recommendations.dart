83b0f3f3dca520008b547d0c28f6365effa4fa6d
sisshmue <sisshmue404@gmail.com>
2026-03-08 15:50:59 +0700

    connect recommendation with movie detail

--- lib/features/recommendation/presentation/pages/recommendations.dart
+++ lib/features/recommendation/presentation/pages/recommendations.dart
@@ -1,3 +1,5 @@
+import 'package:cineflow/features/movie/presentation/bloc/movie_bloc.dart';
+import 'package:cineflow/features/movie/presentation/pages/movie_detail.dart';
 import 'package:cineflow/features/recommendation/presentation/bloc/recommendation_bloc.dart';
 import 'package:cineflow/features/recommendation/presentation/widgets/genre_chip.dart';
 import 'package:cineflow/features/recommendation/presentation/widgets/recommendations_input.dart';
@@ -45,54 +47,83 @@ class _RecommendationsState extends State<Recommendations> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: BlocConsumer<RecommendationBloc, RecommendationState>(
-                builder: (context, state) {
-                  if (state is RecommendationInitial) {
-                    return const Center(
-                      child: Text('Type a genre to get started!'),
-                    );
-                  }
-                  if (state is RecommendationLoading) {
-                    return RecommendationLoadingWidget();
-                  }
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<MovieBloc, MovieState>(
+          listener: (context, state) {
+            if (state is MovieSuccess) {
+              final movieId = state.movieList[0].id;
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MovieDetailPage(movieId: movieId),
+                ),
+              );
+            }
+            if (state is MovieFailure) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.message)));
+            }
+          },
+        ),
+      ],
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: BlocConsumer<RecommendationBloc, RecommendationState>(
+                  builder: (context, state) {
+                    if (state is RecommendationInitial) {
+                      return const Center(
+                        child: Text('Type a genre to get started!'),
+                      );
+                    }
+                    if (state is RecommendationLoading) {
+                      return RecommendationLoadingWidget();
+                    }
 
-                  if (state is RecommendationSuccess) {
-                    return ListView.builder(
-                      padding: const EdgeInsets.all(12),
-                      itemCount: state.recomList.length,
-                      itemBuilder: (context, index) {
-                        final movie = state.recomList[index];
+                    if (state is RecommendationSuccess) {
+                      return ListView.builder(
+                        padding: const EdgeInsets.all(12),
+                        itemCount: state.recomList.length,
+                        itemBuilder: (context, index) {
+                          final movie = state.recomList[index];
 
-                        return RecommendationCard(
-                          title: movie.title,
-                          year: movie.year,
-                          description: movie.description,
-                        );
-                      },
-                    );
-                  }
-                  return SizedBox.shrink();
-                },
-                listener: (context, state) {
-                  if (state is RecommendationFail) {
-                    ScaffoldMessenger.of(
-                      context,
-                    ).showSnackBar(SnackBar(content: Text(state.message)));
-                  }
-                },
+                          return GestureDetector(
+                            onTap: () {
+                              context.read<MovieBloc>().add(
+                                SearchMoviesEvent(keyWord: movie.title),
+                              );
+                            },
+                            child: RecommendationCard(
+                              title: movie.title,
+                              year: movie.year,
+                              description: movie.description,
+                            ),
+                          );
+                        },
+                      );
+                    }
+                    return SizedBox.shrink();
+                  },
+                  listener: (context, state) {
+                    if (state is RecommendationFail) {
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(SnackBar(content: Text(state.message)));
+                    }
+                  },
+                ),
               ),
-            ),
-            if (showChips) GenreChip(sendMessage: _sendMessage),
-            RecommendationsInput(
-              controller: _controller,
-              sendMessage: _sendMessage,
-            ),
-          ],
+              if (showChips) GenreChip(sendMessage: _sendMessage),
+              RecommendationsInput(
+                controller: _controller,
+                sendMessage: _sendMessage,
+              ),
+            ],
+          ),
         ),
       ),
     );
