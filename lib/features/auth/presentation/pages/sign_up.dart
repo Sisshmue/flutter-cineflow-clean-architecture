748d409e3f99df1ea7b788359d3d2db98ad5301d
sisshmue <sisshmue404@gmail.com>
2026-03-09 10:30:35 +0700

    adjust ui for sign up page

--- lib/features/auth/presentation/pages/sign_up.dart
+++ lib/features/auth/presentation/pages/sign_up.dart
@@ -51,91 +51,102 @@ class _SignUpPageState extends State<SignUpPage> {
             if (state is AuthLoading) {
               return Center(child: CircularProgressIndicator());
             }
-            return SingleChildScrollView(
-              child: Form(
-                key: key,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // const Icon(
-                    //   Icons.movie_filter,
-                    //   size: 80,
-                    //   color: Pallete.primaryRed,
-                    // ),
-                    SizedBox(
-                      width: 200,
-                      height: 200,
-                      child: Image.asset(
-                        'assets/logo/app_logo.png',
-                        fit: BoxFit.cover,
-                      ),
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
                     ),
-                    const Text(
-                      "Create Account",
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    AuthField(
-                      hintText: "Full Name",
-                      icon: Icons.person,
-                      controller: _nameController,
-                    ),
-                    const SizedBox(height: 16),
-                    AuthField(
-                      hintText: "Email",
-                      icon: Icons.email,
-                      controller: _emailController,
-                    ),
-                    const SizedBox(height: 16),
-                    AuthField(
-                      hintText: "Password",
-                      icon: Icons.lock,
-                      isPassword: true,
-                      controller: _passwordController,
-                    ),
-                    const SizedBox(height: 24),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Pallete.primaryRed,
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      onPressed: () {
-                        if (key.currentState!.validate()) {
-                          FocusScope.of(context).unfocus();
-                          context.read<AuthBloc>().add(
-                            AuthSignUp(
-                              name: _nameController.text,
-                              email: _emailController.text.trim(),
-                              password: _passwordController.text.trim(),
+                    child: Form(
+                      key: key,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // const Icon(
+                            //   Icons.movie_filter,
+                            //   size: 80,
+                            //   color: Pallete.primaryRed,
+                            // ),
+                            SizedBox(
+                              width: 200,
+                              height: 200,
+                              child: Image.asset(
+                                'assets/logo/app_logo.png',
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          );
-                        }
-                      },
-                      child: const Text(
-                        "SIGN UP",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                            const Text(
+                              "Create Account",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 40),
+                            AuthField(
+                              hintText: "Full Name",
+                              icon: Icons.person,
+                              controller: _nameController,
+                            ),
+                            const SizedBox(height: 16),
+                            AuthField(
+                              hintText: "Email",
+                              icon: Icons.email,
+                              controller: _emailController,
+                            ),
+                            const SizedBox(height: 16),
+                            AuthField(
+                              hintText: "Password",
+                              icon: Icons.lock,
+                              isPassword: true,
+                              controller: _passwordController,
+                            ),
+                            const SizedBox(height: 24),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Pallete.primaryRed,
+                                minimumSize: const Size(double.infinity, 50),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                              ),
+                              onPressed: () {
+                                if (key.currentState!.validate()) {
+                                  FocusScope.of(context).unfocus();
+                                  context.read<AuthBloc>().add(
+                                    AuthSignUp(
+                                      name: _nameController.text,
+                                      email: _emailController.text.trim(),
+                                      password: _passwordController.text.trim(),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: const Text(
+                                "SIGN UP",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text(
+                                "Already have an account? Log In",
+                                style: TextStyle(color: Pallete.textSecondary),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text(
-                        "Already have an account? Log In",
-                        style: TextStyle(color: Pallete.textSecondary),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             );
           },
         ),
